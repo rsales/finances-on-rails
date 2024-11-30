b76b36feac306be7d420e874ecff6393996d1e3d
rsales <rafael.sales93@hotmail.com>
2024-11-30 14:54:05 -0300

    Importing vue into the project with importmap

---
+++ app/javascript/vue_app.js
@@ -0,0 +1,10 @@
+// import { createApp, ref } from 'vue'
+
+// createApp({
+//   setup() {
+//     const message = ref('Hello Vue!')
+//     return {
+//       message
+//     }
+//   }
+// }).mount('#app')
